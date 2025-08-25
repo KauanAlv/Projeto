83400aa885f6dd54212826beb2597a578818378b
KAUAN ALVES PEREIRA <25203696@CFP127ALUNO.LOCAL>
2025-08-25 16:25:31 -0300

    Finalizada a aula sobre GIT

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Fala chat");
 		System.out.println("Estamos no SENAI!!");
+		System.out.println("E o git deu certo!!!");
 	}
 }
